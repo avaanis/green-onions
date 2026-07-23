d87e1fb2529bc5ecf740ad13cab65f37104f510d
as <ashleelshawarma31@gmail.com>
2026-07-23 03:08:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,16 @@
 function checkPassword(){
-
-
 let password =
 document.getElementById("password").value;
-
-
-
 if(password.toUpperCase() === "MUSTARDGAS"){
-
-
 document.getElementById("message").innerHTML =
 "ACCESS GRANTED... RECOVERING FILE...";
-
-
 setTimeout(function(){
-
-
 window.location.href =
 "https://canva.link/bt0uj3aafq9ymxf";
-
-
 },1500);
-
-
-
 }
-
 else{
-
-
 document.getElementById("message").innerHTML =
 "ACCESS DENIED // INVALID CHEMICAL IDENTIFIER";
-
-
 }
-
-
-
 }
